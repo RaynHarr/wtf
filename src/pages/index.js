b25ddbede9025dfa69819d33f46a8d75a73a79ae
Kiren Srinivasan <srinitude@gmail.com>
2018-08-04 10:13:34 -0700

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Kiren's React Project</h1>
+    <p>Weekend workshop for Kiren to learn about React and design more in general.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
