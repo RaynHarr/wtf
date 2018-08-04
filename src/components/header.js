009aba8e239acf0efb032b3f6b4c21bccc4ea83c
Kiren Srinivasan <srinitude@gmail.com>
2018-08-04 16:09:48 -0700

    Makes Header component listen to window's scroll event

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,37 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/"><img src={require('../../static/images/logo-designcode.svg')} /></Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/downloads">Downloads</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/workshops"><button>Buy</button></Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+    console.log(scrollTop)
+  }
+
+  render() {
+    return (
+      <div className="Header">
+        <div className="HeaderGroup">
+          <Link to="/"><img src={require('../../static/images/logo-designcode.svg')} /></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/workshops"><button>Buy</button></Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
