5c6520f109cb4c3350101669d145b99680c4b02e
Kiren Srinivasan <srinitude@gmail.com>
2018-08-05 13:59:53 -0700

    Adds Contentful connection to GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   siteMetadata: {
     title: "Kiren's React Project",
+    description: "My awesome site",
+    keywords: "react, designers"
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'tjm9ck61gz6q',
+        accessToken: 'cf064b24417d0539dd3ce11d7c1ac5f85080dfb5117dda97e34734704ebd63a0'
+      }
+    }
+  ],
 }
